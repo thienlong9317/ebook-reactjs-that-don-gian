a659082a6c43b57e05dc0607540787cea6ccb941
Long <46297826+sarsmustu@users.noreply.github.com>
2023-03-18 11:28:21 +0700

    first commit

--- phan4/todo-app-starter/src/index.js
+++ phan4/todo-app-starter/src/index.js
@@ -2,5 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const element = <h1>Hello from VNTALKING</h1>;
-
+//test first git
 ReactDOM.render(element, document.getElementById("root"));
